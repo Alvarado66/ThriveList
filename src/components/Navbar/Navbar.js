75fa90eddceabe50fce50ea7d069b65bb4a89862
Fernando Alvarado- <Fernandoahd@icloud.com>
2021-02-04 00:11:44 -0500

    installing router dom

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link to="/" className="navbar logo">Thriver</Link>
+                    <div className="menu-icon">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
