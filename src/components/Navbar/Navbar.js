ba7fd4d6386fa2b69bf03555549ebcce44c4493f
Fernando Alvarado- <Fernandoahd@icloud.com>
2021-02-05 21:25:43 -0500

    added hooks for state

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdSpa } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
-    return (
-        <>
-            <div className="navbar">
-                <div className="navbar-container container">
-                    <Link to="/" className="navbar logo">Thriver</Link>
-                    <div className="menu-icon">
-                        
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  // A hook to define 'click' with initial value of false
+  const [click, setClick] = useState(false);
+
+  // This will handle the click similar to a toggle
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <Link to="/" className="navbar logo">
+            Thriver
+            <MdSpa className="navbar-icon" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {/* if the hamburger menu is clicked the icons change */}
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
